55195c53da36d9b461b12d4fec1bbccb24db1470
mariosknl <mariosknl@outlook.com>
2020-05-06 11:10:05 +0300

    Editing Section 1:done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'users/new'
   get 'users/create'
-  resources :users, only: [:new, :create]
+  get 'user/edit'
+  resources :users, only: [:new, :create, :edit, :update]
 end
